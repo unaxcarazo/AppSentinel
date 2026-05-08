305ab0783c7ee3fba368297a9c1a0fb16b9b3247
hiemnmdhip-it <hiemmariangel.nain.munoz@zabalburu.org>
2026-05-08 10:23:55 +0200

    Agregado AppSentinel con su estructura de capas

---
+++ AppSentinel/src/main/java/org/appsentinel/AppSentinel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.appsentinel;
+
+/**
+ *
+ * @author DAW1
+ */
+public class AppSentinel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
